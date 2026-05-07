dbace0c9272a0cb808dfcc8eb87ca79e6d386f2c
rishikabhandari931 <rishikabhandari931@gmail.com>
2026-05-07 17:20:50 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -23,7 +23,7 @@ function App() {
 
   const handlePredict = async () => {
     try {
-      const response = await fetch("http://127.0.0.1:5000/predict", {
+      const response = await fetch("https://heart-disease-prediction-app-1-kk6z.onrender.com/predict", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form)
